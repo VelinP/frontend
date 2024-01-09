62048185091ede72f9091f396d74a87a905ee317
VelinP <dottallot1@hotmail.com>
2024-01-09 23:43:36 +0200

    small

--- src/util/requester.ts
+++ src/util/requester.ts
@@ -1,7 +1,12 @@
+interface data{
+    xd:string,
+}
+
 export const get = async (url:string) =>{
     try{
         const data = await fetch(url);
         const processeddata = await data.json()
+        console.log(`processed data->${processeddata}`)
         return processeddata
 
     }catch(e){
