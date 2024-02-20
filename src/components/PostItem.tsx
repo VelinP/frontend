2f1f37792f9c4b7102aa543c0e7a3241791b074e
VelinP <dottallot1@hotmail.com>
2024-02-20 23:38:53 +0200

    Began work on adding item + fixed some backend

---
+++ src/components/PostItem.tsx
@@ -0,0 +1,38 @@
+import * as auth from '../util/authservice'
+import { FC } from "react"
+
+export const PostItem:FC = () =>{
+  
+    interface InfoInterface{
+        
+        password:string | File,
+        email:string | File
+    }
+
+    const logger = (event:any):void =>{
+        event.preventDefault()
+        const { password , email } = Object.fromEntries(new FormData(event.target));
+        const data:InfoInterface = {password,email};
+        auth.login("http://localhost:4000/login", data)
+    }
+    
+
+    return(
+        <div className="submitdiv">
+            <div className="submitholderdiv">
+            <h1>Login</h1>
+
+                <form id="create" onSubmit={logger}>
+                <input name="username" id="description" placeholder="username"></input>
+                <input name="password" id="description" placeholder="password"></input>
+                <input name="email" id="description" placeholder="email"></input>
+                <button>Submit</button>
+                </form>
+            </div>
+
+        </div>
+        
+        
+    )
+
+}
